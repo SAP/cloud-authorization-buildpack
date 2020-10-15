2fe977b712ca2598299cc9420370d2ad1d6236a1
Felix Blass <felix.blass@sap.com>
2020-10-15 14:06:10 +0200

    Add REUSE 3.0 license information

--- fixtures/node_with_opa/index.js
+++ fixtures/node_with_opa/index.js
@@ -1,4 +1,6 @@
-
+// SPDX-FileCopyrightText: 2020 2020 SAP SE or an SAP affiliate company and Cloud Security Client Go contributors
+//
+// SPDX-License-Identifier: Apache-2.0
 
 setInterval(() => {
 
